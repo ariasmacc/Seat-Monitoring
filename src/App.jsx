5639614732200b889398471a2719ea7ff5cc569b
ariasmacc <ariacaden10@gmail.com>
2025-11-17 15:58:37 +0800

    FINAL NGROK URL UPDATE for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SettingsPage from './pages/SettingsPage/SettingsPage.jsx'; 
 import './App.css'; // Gagamitin natin 'to para sa styling
 
-// ITO ANG TAMANG ILAGAY
-const API_URL = 'https://spinachlike-amada-nonpositivistic.ngrok-free.app/status';
+const API_URL = 'https://spinachlike-amada-nonpositivistic.ngrok-free.dev/status';
 // --- Ating Bagong Home Page ---
 function LiveStatusPage() {
   const [seats, setSeats] = useState([]); // Dito i-store ang [ {id: 1, status: 'Available', ...}, ... ]
